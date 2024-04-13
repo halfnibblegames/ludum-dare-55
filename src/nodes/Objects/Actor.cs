3113428edf7d4dc8bd205affa16ee5c901944ab4
William Barbosa <heytherewill@gmail.com>
2024-04-13 21:51:31 +0200

    🎞️ Convention based actor animation

--- src/nodes/Objects/Actor.cs
+++ src/nodes/Objects/Actor.cs
@@ -8,8 +8,11 @@ public abstract class Actor : KinematicBody2D
     private TileMap? tileMap;
     protected bool IsActive { get; private set; }
     protected abstract float Speed { get; }
+    
     private Vector2 velocity;
-
+    private string lastAnimation;
+    private bool lastShouldBeMirrored;
+    
     protected Vector2 FindCurrentTile()
     {
         if (tileMap is null) throw new InvalidOperationException();
@@ -54,14 +57,44 @@ public abstract class Actor : KinematicBody2D
     {
         base._Process(delta);
 
-        // TODO: update animation here based on velocity
+        var animatedSprite = GetNode<AnimatedSprite>("AnimatedSprite");
+        var newAnimation = CalculateAnimation();
+        if (newAnimation != lastAnimation)
+        {
+            animatedSprite.Animation = newAnimation;
+        }
+
+        animatedSprite.FlipH = lastShouldBeMirrored = checkIfSpriteShouldBeMirrored();
+    }
+
+    private bool checkIfSpriteShouldBeMirrored()
+    {
+        // If we're idle then we should not mirror.
+        if (velocity is { x: 0, y: 0 })
+            return lastShouldBeMirrored;
+
+        return velocity.x < 0;
+    }
+
+    protected virtual string CalculateAnimation()
+    {
+        // We're standing still.
+        if (velocity is { x: 0, y: 0 })
+            return "idle";
+        
+        // We're moving straight up or down
+        if (velocity.x == 0)
+            return velocity.y > 0 ? "south" : "north";
+
+        // If we're going up, use the diagonal up animation, otherwise all horizontal movement is the same.
+        return velocity.y >= 0 ? "horizontal" : "diagonal";
     }
 
     public override void _PhysicsProcess(float delta)
     {
-        if (!IsActive) return;
-
         velocity = Vector2.Zero;
+        
+        if (!IsActive) return;
 
         velocity.x = Input.GetActionStrength("move_right") - Input.GetActionStrength("move_left");
         velocity.y = Input.GetActionStrength("move_down") - Input.GetActionStrength("move_up");
