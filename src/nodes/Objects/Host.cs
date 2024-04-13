66ccc1847e51110922514212d82c2730e08d21aa
William Barbosa <heytherewill@gmail.com>
2024-04-13 21:51:47 +0200

    🐦‍⬛ THE CONJURING

--- src/nodes/Objects/Host.cs
+++ src/nodes/Objects/Host.cs
@@ -6,6 +6,7 @@ namespace HalfNibbleGame.Objects;
 
 public class Host : Actor
 {
+    private const float summoningDuration = 0.7f;
     public const double MaxMadness = 200;
 
     [Export] private float speed = 100;
@@ -13,7 +14,9 @@ public class Host : Actor
     protected override float Speed => speed;
 
     public float Madness { get; private set; }
-
+    
+    private float summoningTimeout = 0.0f;
+    
     public override void _Ready()
     {
         Global.Services.ProvideInScene(this);
@@ -29,11 +32,29 @@ public class Host : Actor
     {
         base._Process(delta);
 
-        if (!IsActive) return;
+        
+        if (summoningTimeout > 0.0f)
+        {
+            summoningTimeout -= delta;
+            if (summoningTimeout <= 0.0f)
+            {
+                Global.Services.Get<WorldManager>().SummonForm(FindCurrentTile() + Vector2.Right);
+            }
+        }
+        else
+        {
+            summoningTimeout = 0.0f;
+        }
 
+        if (!IsActive) return;
+            
         if (Input.IsActionJustPressed("summon_imp"))
         {
-            Global.Services.Get<WorldManager>().SummonForm(FindCurrentTile() + Vector2.Right);
+            Suspend();
+            summoningTimeout = summoningDuration;
         }
     }
+
+    protected override string CalculateAnimation() 
+        => summoningTimeout > 0.0f ? "summoning" : base.CalculateAnimation();
 }
