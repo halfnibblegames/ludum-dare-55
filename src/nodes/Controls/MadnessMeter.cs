64bfeda42fb179975f79c751dbe0c7b420759291
Tom Rijnbeek <tom@tomrijnbeek.me>
2024-04-13 16:21:05 +0200

    😶‍🌫️ Add chromatic aberration based on madness

--- src/nodes/Controls/MadnessMeter.cs
+++ src/nodes/Controls/MadnessMeter.cs
@@ -5,21 +5,26 @@ namespace HalfNibbleGame.Controls;
 
 public class MadnessMeter : TextureRect
 {
-    private const int FullMadnessBarSize = 704;
+    private const int fullMadnessBarSize = 704;
+    private const float maxAberration = 5;
 
     public override void _Process(float delta)
     {
         // TODO: Make this less disgusting by using events or some other way of observing the player.
         var madnessBar = GetNode<ColorRect>("../MadnessBar");
         var portrait = GetNode<TextureRect>("../PlayerPortrait");
-        var player = GetNode<Player>("../ViewportContainer/Viewport/World/SandboxLevel/Player");
+        var player = GetNode<Player>("../ViewportContainer/Viewport/World/Player");
+        var material = GetNode<ColorRect>("../CanvasLayer/PostProcess").Material as ShaderMaterial;
         Visible = portrait.Visible = madnessBar.Visible = player is not null;
 
         if (player is null)
             return;
 
         var madnessPercentage = player.Madness / Player.MaxMadness;
-        madnessBar.RectSize = madnessBar.RectSize with { x = (int)(FullMadnessBarSize * madnessPercentage) };
+
+        madnessBar.RectSize = madnessBar.RectSize with { x = (int)(fullMadnessBarSize * madnessPercentage) };
         madnessBar.Color = player.CurrentSpirit?.Color ?? madnessBar.Color;
+
+        material?.SetShaderParam("amount", madnessPercentage * maxAberration);
     }
 }
