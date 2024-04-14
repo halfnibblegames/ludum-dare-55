a63b7eb7765b836ebd1e7dc5c70946e2c6f9e03c
Tom Rijnbeek <tom@tomrijnbeek.me>
2024-04-14 23:05:05 +0200

    🔳 Add pressure plates

--- src/nodes/Objects/Level/DetectingArea2D.cs
+++ src/nodes/Objects/Level/DetectingArea2D.cs
@@ -2,12 +2,23 @@
 
 namespace HalfNibbleGame.Objects.Level;
 
-public abstract class DetectingArea2D : Area2D
+public abstract class DetectingArea2D : Area2D, ILevelResettable
 {
+    private bool signalsConnected;
+    private Actor? currentActor;
+
     public override void _Ready()
     {
         base._Ready();
+        AddToGroup(Constants.LevelResetGroup);
+    }
+
+    public void Reset()
+    {
+        if (signalsConnected) return;
         Connect("body_entered", this, nameof(onBodyEntered));
+        Connect("body_exited", this, nameof(onBodyExited));
+        signalsConnected = true;
     }
 
     private void onBodyEntered(PhysicsBody2D body)
@@ -15,8 +26,19 @@ public abstract class DetectingArea2D : Area2D
         if (body is Actor { HasControl: true } actor)
         {
             OnActorEntered(actor);
+            currentActor = actor;
+        }
+    }
+
+    private void onBodyExited(PhysicsBody2D body)
+    {
+        if (body == currentActor)
+        {
+            OnActorExited(currentActor);
+            currentActor = null;
         }
     }
 
     protected abstract void OnActorEntered(Actor actor);
+    protected virtual void OnActorExited(Actor actor) { }
 }
---
+++ src/nodes/Objects/Level/PressurePlate.cs
@@ -0,0 +1,47 @@
+﻿using Godot;
+using HalfNibbleGame.Systems;
+
+namespace HalfNibbleGame.Objects.Level;
+
+public class PressurePlate : DetectingArea2D, ILevelState
+{
+    private const string unpressedAnimation = "idle";
+    private const string pressedAnimation = "Active";
+
+    [Export] public ChannelKey Channel;
+
+    private ChannelState state;
+    private AnimatedSprite sprite = null!;
+
+    public override void _Ready()
+    {
+        base._Ready();
+        AddToGroup(Constants.LevelStateGroup);
+        sprite = GetNode<AnimatedSprite>("AnimatedSprite");
+    }
+
+    protected override void OnActorEntered(Actor actor)
+    {
+        if (actor is not Imp)
+        {
+            this.UpdateChannel(Channel, ChannelState.On);
+            sprite.Animation = pressedAnimation;
+        }
+    }
+
+    protected override void OnActorExited(Actor actor)
+    {
+        if (actor is not Imp)
+        {
+            this.UpdateChannel(Channel, ChannelState.Off);
+            sprite.Animation = unpressedAnimation;
+        }
+    }
+
+    public void ConsumeChange(ChannelKey key, ChannelState newState)
+    {
+        if (key != Channel) return;
+
+        state = newState;
+    }
+}
