552d55e6bec7eb92df6a8954c67adc9b751b64f1
Tom Rijnbeek <tom@tomrijnbeek.me>
2024-04-14 16:15:02 +0200

    🏝️ Fix level 1 and add exit

--- src/nodes/Autoload/Prefabs.cs
+++ src/nodes/Autoload/Prefabs.cs
@@ -6,6 +6,7 @@ namespace HalfNibbleGame.Autoload;
 [UsedImplicitly]
 public sealed class Prefabs : Node
 {
+    [Export] public PackedScene? Sandbox;
     [Export] public PackedScene? Imp;
 
     public override void _Ready()
--- src/nodes/Objects/Level/Exit.cs
+++ src/nodes/Objects/Level/Exit.cs
@@ -6,13 +6,13 @@ namespace HalfNibbleGame.Objects.Level;
 
 public class Exit : DetectingArea2D
 {
-    [Export] public PackedScene NextLevelScene = null!;
+    [Export] public PackedScene? NextLevelScene;
 
     protected override void OnActorEntered(Actor actor)
     {
-        if (actor is Host)
-        {
-            Global.Services.Get<WorldManager>().CallDeferred(nameof(WorldManager.LoadLevel), NextLevelScene);
-        }
+        if (actor is not Host) return;
+        var nextLevel = NextLevelScene ?? Global.Prefabs.Sandbox;
+        if (nextLevel is null) return;
+        Global.Services.Get<WorldManager>().CallDeferred(nameof(WorldManager.LoadLevel), nextLevel);
     }
 }
