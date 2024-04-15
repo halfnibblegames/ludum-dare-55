edff4bfa00e47deba08a7f6468a71b95902d2736
William Barbosa <heytherewill@gmail.com>
2024-04-15 12:21:59 +0200

    🎶 Habemus Musica

--- src/nodes/Title.cs
+++ src/nodes/Title.cs
@@ -3,6 +3,13 @@ using HalfNibbleGame;
 
 public class Title : Control
 {
+    public override void _Ready()
+    {
+        base._Ready();
+
+        GetNode<AudioStreamPlayer>("AudioStreamPlayer").Play();
+    }
+
     public void ToggleSpeedrunTimer(bool showSpeedrunTimer)
     {
         Settings.ShowTimer = showSpeedrunTimer;
